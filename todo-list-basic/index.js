6fe2b3199ad4ab7c8f6f78f4c77fff45a815701e
Ricardo Brito <r.cardobrito@gmail.com>
2025-03-18 16:52:39 -0300

    Implemente do alert para que nao deixe o usuário adicionar uma tarega em branco.

--- todo-list-basic/index.js
+++ todo-list-basic/index.js
@@ -2,24 +2,30 @@
 // querySelector seleciona um elemento do html (ex: a class declarada no button - '.button-add-task')
 const button = document.querySelector('.button-add-task');
 const input = document.querySelector('.input-task');
-const completeList = document.querySelector('.list-tasks')
+const completeList = document.querySelector('.list-tasks');
 
 // declaração de array para salvar tarefas inputadas pelo usuário.
-let myItemList = []
+let myItemList = [];
 
 // Função para captar o input da tarefa e salvar no array [myItemList]
 function addNewTask() {
+  // Verifica se o campo de input está vazio
+  if (input.value.trim() === '') {
+    alert("Por favor, insira uma nova tarefa!");
+    return; // RRetorna para evitar adicionar uma lista vazia
+  }
+
   myItemList.push({
     task: input.value,
     done: false
   });
-  input.value = ''
-  showTasks();
+  input.value = ''; // Limpa o campo de entrada
+  showTasks(); // Atualiza a exibição das tarefas
 }
 
 
 function showTasks() {
-  let newLi = ''
+  let newLi = '';
 
   myItemList.forEach((item, index) => {
     newLi = newLi + `
@@ -37,34 +43,34 @@ function showTasks() {
   completeList.innerHTML = newLi;
 
   // salva dados no local storage do navegador
-  localStorage.setItem('list', JSON.stringify(myItemList))
+  localStorage.setItem('list', JSON.stringify(myItemList));
 
 }
 
 
 function doneTask(index) {
-  myItemList[index].done = !myItemList[index].done
+  myItemList[index].done = !myItemList[index].done;
 
-  showTasks()
+  showTasks();
 }
 
 
 function deleteItem(index) {
   myItemList.splice(index, 1);
-  showTasks()
+  showTasks();
 }
 
 function reloadTasks() {
-  const localStorageTasks = localStorage.getItem('list')
+  const localStorageTasks = localStorage.getItem('list');
 
   if (localStorageTasks) {
-    myItemList = JSON.parse(localStorageTasks)
+    myItemList = JSON.parse(localStorageTasks);
   }
 
-  showTasks()
+  showTasks();
 }
 
-reloadTasks()
+reloadTasks();
 
 // addEventListener fica de olho nas interacões com o botão, como declarado abaixo, no click do botão.
 // E chama a função declarada acima para adicionar nova tarefa.
