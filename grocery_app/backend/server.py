7a0bc0c0ea9271d39f753db19f30c696f18f30cb
Ricardo Brito <r.cardobrito@gmail.com>
2025-03-22 19:19:30 -0300

    Correção de sintaxe.

--- grocery_app/backend/server.py
+++ grocery_app/backend/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 connection = get_sql_connection()
 
-@app.route('getUOM', methods=['GET'])
+@app.route('/getUOM', methods=['GET'])
 def get_uom():
   response = uom_dao.get_uoms(connection)
   response = jsonify(response)
@@ -23,7 +23,7 @@ def get_products():
   response.headers.add('Access-Control-Allow-Origin', '*')
   return response
 
-@app.route('/deleteProduct', methods=[POST])
+@app.route('/deleteProduct', methods=['POST'])
 def delete_product():
   return_id = products_dao.delete_product(connection, request.form['product_id'])
   response = jasonify({
