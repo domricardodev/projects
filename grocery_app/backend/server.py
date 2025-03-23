f5d6cdbd077479a16425d968137954c3f7d9c1e1
Ricardo Brito <r.cardobrito@gmail.com>
2025-03-23 08:31:17 -0300

    Implementado a função de salvar um novo produto no banco de dados.

--- grocery_app/backend/server.py
+++ grocery_app/backend/server.py
@@ -17,13 +17,22 @@ def get_uom():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-
 @app.route('/getProducts', methods=['GET'])
 def get_products():
   products = products_dao.get_all_products(connection)
   response = jsonify(products)
   response.headers.add('Access-Control-Allow-Origin', '*')
   return response
+
+@app.route('/insertProduct', methods=['POST'])
+def insert_product():
+   request_payload = json.loads(request.form['data'])
+   product_id = products_dao.insert_new_product(connection, request_payload)
+   response = jsonify({
+      'product_id': product_id
+   })
+   response.headers.add('Access-Control-Allow-Origin', '*')
+   return response
 
 @app.route('/deleteProduct', methods=['POST'])
 def delete_product():
